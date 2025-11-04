89ae41a8094047e6d3411a5ac15967786fbcd82b
SolidSoups <eli.marc.brown@gmail.com>
2025-11-04 01:07:43 +0100

    first commit

---
+++ build/CMakeFiles/PearlEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PearlEngine.dir/link.d"
+  "CMakeFiles/PearlEngine.dir/main.cpp.o"
+  "CMakeFiles/PearlEngine.dir/main.cpp.o.d"
+  "PearlEngine"
+  "PearlEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PearlEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/game_engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_engine.dir/link.d"
+  "CMakeFiles/game_engine.dir/src/main.cpp.o"
+  "CMakeFiles/game_engine.dir/src/main.cpp.o.d"
+  "game_engine"
+  "game_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
