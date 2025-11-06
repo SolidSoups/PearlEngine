e5c076e4eb6c677b6f1121066368189ea7e17fc9
SolidSoups <eli.marc.brown@gmail.com>
2025-11-06 23:46:17 +0100

    added some usufual IDE configuration for quickly building the project with cmake

---
+++ .nvim.lua
@@ -0,0 +1,6 @@
+print("Welcome, to Pearl Engine! 🦖");
+vim.keymap.set("n", "<leader>bb", ":!cmake --build build<CR>", { desc = "[Pearl] Build project" })
+vim.keymap.set("n", "<leader>r", ":!./build/game_engine<CR>", { desc = "[Pearl] Run executable" })
+vim.keymap.set("n", "<leader>br", ":!./build_run.sh<CR>", { desc = "[Pearl] Build and run" })
+vim.keymap.set("n", "<leader>cr", ":!./compile_build_run.sh<CR>", { desc = "[Pearl] Compile, build and run" })
+vim.keymap.set("n", "<leader>bc", ":!./clean_build.sh<CR>", { desc = "[Pearl] Clean build" })
