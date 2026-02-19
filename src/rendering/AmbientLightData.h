a055357d7c7fee745e80c2e98c61216fd3be5144
SolidSoups <eli.marc.brown@gmail.com>
2026-02-19 16:02:46 +0100

    chore: override default ambient light settings

--- src/rendering/AmbientLightData.h
+++ src/rendering/AmbientLightData.h
@@ -3,7 +3,7 @@
 #include "glm/ext/vector_float4.hpp"
 
 struct AmbientLightData{
-  glm::vec4 color = glm::vec4(0.2f, 0.2f, 0.2f, 1.0f);
+  glm::vec4 color = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
   float intensity = 1.0f;
 
   AmbientLightData() = default;
