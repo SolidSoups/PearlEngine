3984f9b659eb6aabb693e7c23d5fd134a47e39ad
SolidSoups <eli.marc.brown@gmail.com>
2026-03-13 01:07:41 +0100

    Winning able to go back to menu

--- assets/scripts/Win.lua
+++ assets/scripts/Win.lua
@@ -1,6 +1,26 @@
+local refs = {}
+function OnCreate()
+	local winTextEnt = Scene.FindEntityByName("YOU WIN")
+	local goBackTextEnt = Scene.FindEntityByName("BACK TO MENU")
+
+	refs.winText = Scene.GetText(winTextEnt)
+	refs.goBackText = Scene.GetText(goBackTextEnt)
+
+	refs.winText.isButton = false
+	refs.winText.isVisible = false
+	refs.goBackText.isButton = false
+	refs.goBackText.isVisible = false
+	refs.goBackText.onClick = function()
+		Scene.LoadScene("assets/MainMenu.json")
+	end
+end
+
 function OnCollisionEnter(other, normal, pen)
 	local otherName = Scene.GetNameComp(other).name
 	if otherName == "Player" then
-		Debug.Log("YOU WIN!!!")
+		refs.winText.isButton = true
+		refs.winText.isVisible = true
+		refs.goBackText.isButton = true
+		refs.goBackText.isVisible = true
 	end
 end
